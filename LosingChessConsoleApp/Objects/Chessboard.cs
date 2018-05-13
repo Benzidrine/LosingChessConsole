9bbe13aa15db7d6d291f3919dd241eb48b0027fa
MechatronicsGuyPAL <MechatronicsGuy_PAL@hotmail.com>
2018-05-13 16:39:11 +0800

    Indexing and Pieces
    switched to 1 indexing,  started pieces code

--- LosingChessConsoleApp/Objects/Chessboard.cs
+++ LosingChessConsoleApp/Objects/Chessboard.cs
@@ -16,10 +16,32 @@ namespace LosingChessConsoleApp.Models
 
         int[,] ChessboardArray = new int[,] { };
 
+        public bool IsOdd( int num)
+        {
+            return (num % 2 == 0) ? false : true;
+        }
+
+        public bool MovePiece()
+        {
+            return true;
+        }
 
         public  bool IsSquareBlack (int x, int y)
         {
-            return true;
+            bool returnVal = false;
+            if (IsOdd(x))
+            {
+                if (!IsOdd(y))
+                {
+                    returnVal = true;
+                }
+            }
+            else if (IsOdd(y))
+            {
+                returnVal = true;
+            }
+            
+            return returnVal;
         }
 
 
@@ -27,14 +49,14 @@ namespace LosingChessConsoleApp.Models
         {
             switch (x)
             {
-                case 0: return "A";
-                case 1: return "B";
-                case 2: return "C";
-                case 3: return "D";
-                case 4: return "E";
-                case 5: return "F";
-                case 6: return "G";
-                case 7: return "H";
+                case 1: return "A";
+                case 2: return "B";
+                case 3: return "C";
+                case 4: return "D";
+                case 5: return "E";
+                case 6: return "F";
+                case 7: return "G";
+                case 8: return "H";
                 default: return "0";
             }
         }
@@ -43,46 +65,46 @@ namespace LosingChessConsoleApp.Models
         {
             switch (y)
             {
-                case 0: return "8";
-                case 1: return "7";
-                case 2: return "6";
-                case 3: return "5";
-                case 4: return "4";
-                case 5: return "3";
-                case 6: return "2";
-                case 7: return "1";
+                case 1: return "8";
+                case 2: return "7";
+                case 3: return "6";
+                case 4: return "5";
+                case 5: return "4";
+                case 6: return "3";
+                case 7: return "2";
+                case 8: return "1";
                 default: return "0";
             }
         }
 
-        private  int BoardXAxis(String x)
+        private  int BoardXAxis(string x)
         {
             switch (x)
             {
-                case "A": return 0;
-                case "B": return 1;
-                case "C": return 2;
-                case "D": return 3;
-                case "E": return 4;
-                case "F": return 5;
-                case "G": return 6;
-                case "H": return 7;
+                case "A": return 1;
+                case "B": return 2;
+                case "C": return 3;
+                case "D": return 4;
+                case "E": return 5;
+                case "F": return 6;
+                case "G": return 7;
+                case "H": return 8;
                 default: return 0;
             }
         }
 
-        private  int BoardYAxis(String y)
+        private  int BoardYAxis(string y)
         {
             switch (y)
             {
-                case "8": return 0;
-                case "7": return 1;
-                case "6": return 2;
-                case "5": return 3;
-                case "4": return 4;
-                case "3": return 5;
-                case "2": return 6;
-                case "1": return 7;
+                case "8": return 1;
+                case "7": return 2;
+                case "6": return 3;
+                case "5": return 4;
+                case "4": return 5;
+                case "3": return 6;
+                case "2": return 7;
+                case "1": return 8;
                 default: return 0;
             }
         }
