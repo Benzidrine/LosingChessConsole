8fd6550bc2dd8a0b1f6a95c7e880c9c60909cc2f
MechatronicsGuyPAL <MechatronicsGuy_PAL@hotmail.com>
2018-11-14 19:57:11 +0800

    Added methods, and minor changes
    added SetPath(), PathIsClear(), and Position methods

--- LosingChessConsoleApp/Objects/Position.cs
+++ LosingChessConsoleApp/Objects/Position.cs
@@ -8,10 +8,55 @@ namespace LosingChessConsoleApp.Models
 {
     public class Position
     {
+
+        public Position()
+        {
+            X = 0;
+            Y = 0;
+        }
+
         public Position (int x, int y)
         {
             X = x;
             Y = y;
+        }
+
+        public Position (Position pos)
+        {
+            X = pos.X;
+            Y = pos.Y;
+        }
+
+        // makes the position equal to the difference between two positions
+        public void Diff(Position pos, Position newPos)
+        {
+            X = (pos.X - newPos.X);
+            Y = (pos.Y - newPos.Y);
+        }
+
+        public void Add(Position pos)
+        {
+            X += pos.X;
+            Y += pos.Y;
+        }
+
+        // makes the position a unit vector
+        public void Sign(Position pos)
+        {
+            if (pos.X != 0)
+            {
+                X = (pos.X / (Math.Abs(pos.X)));
+            }
+            else { X = 0;}
+            if (pos.Y != 0)
+            {
+                Y = (pos.Y / (Math.Abs(pos.Y)));
+            }
+            else
+            {
+                Y = 0;
+            }
+            
         }
 
         public int X { get; set; }
